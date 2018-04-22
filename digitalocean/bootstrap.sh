9b71d9f0a96ec81028f7a748860bd2ff61ca8d9e
Mario Juric <mjuric@astro.washington.edu>
2018-04-22 02:35:15 -0700

    WIP

--- digitalocean/bootstrap.sh
+++ digitalocean/bootstrap.sh
@@ -4,6 +4,8 @@
 # hosts starts.
 #
 
+set -xe
+
 #
 # set up firewall and enable it
 #
@@ -15,7 +17,8 @@ cp config/kafka.xml /etc/firewalld/services/
 cp config/ztf-trusted.xml /etc/firewalld/zones/
 
 firewall-cmd --zone=public --change-interface=eth0 --permanent
-systemctl reload firewalld
+firewall-cmd --zone=trusted --change-interface=eth1 --permanent
+systemctl restart firewalld
 
 #
 # Add swap space. mirrormaker runs out of memory otherwise.
@@ -35,20 +38,20 @@ yum install -y java
 #yum install -y confluent-platform-oss-2.11
 yum install -y confluent-kafka-2.11
 
+## DO Droplet Metadata Service:https://www.digitalocean.com/community/tutorials/an-introduction-to-droplet-metadata
+# PRIVATE_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
+# PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+# HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
+## More generic, but sensitive to parsing
 PRIVATE_IP=$(ifconfig eth1 | grep "inet " | awk '{print $2}')
 PUBLIC_IP=$(ifconfig eth0 | grep "inet " | awk '{print $2}')
+HOSTNAME=$(hostname)
+REPLICA=${HOSTNAME#kafka}
 
 sed "s/\$PRIVATE_IP/$PRIVATE_IP/" config/zookeeper.properties > /etc/kafka/zookeeper.properties
-echo "1" > /var/lib/zookeeper/myid
-
-systemctl start confluent-zookeeper
-systemctl enable confluent-zookeeper
+echo "$REPLICA" > /var/lib/zookeeper/myid
 
 cp config/server.properties /etc/kafka
-
-systemctl start confluent-kafka
-systemctl enable confluent-kafka
-
 
 #
 # Set up systemd services that will start mirrormaker and kafka
@@ -58,5 +61,16 @@ cp config/{ipac,uw}.properties /etc/ztf
 cp config/ztf-mirrormaker.service /etc/systemd/system/
 
 systemctl daemon-reload
-systemctl start ztf-mirrormaker
-systemctl enable ztf-mirrormaker
+
+#
+# Enable and start it all up
+#
+
+#systemctl start confluent-zookeeper
+#systemctl enable confluent-zookeeper
+#
+#systemctl start confluent-kafka
+#systemctl enable confluent-kafka
+#
+#systemctl start ztf-mirrormaker
+#systemctl enable ztf-mirrormaker
