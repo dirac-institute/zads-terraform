3e8c49be7d751f95c4d6ebbbca04af8c1d392919
Mario Juric <mjuric@astro.washington.edu>
2018-04-22 22:58:17 -0700

    WIP

--- digitalocean/bootstrap.sh
+++ digitalocean/bootstrap.sh
@@ -7,6 +7,20 @@
 set -xe
 
 #
+# prepare the kafka data (log) volume
+#
+
+if file -sLb /dev/sda | grep filesystem; then
+	echo "Filesystem already exists on /dev/sda; preserving"
+else
+	mkfs.ext4 -F /dev/sda
+fi
+
+mkdir -p /kafka-data
+echo "/dev/sda /kafka-data ext4 defaults,nofail,discard 0 0" >> /etc/fstab
+mount /kafka-data
+
+#
 # set up firewall and enable it
 #
 yum -y install firewalld
@@ -29,28 +43,27 @@ echo "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
 swapon -a
 
 #
-# Install the application (kafka)
+# Install and configure kafka, zookeeper, and mirrormaker
 #
 rpm --import https://packages.confluent.io/rpm/4.1/archive.key
 cp config/confluent.repo /etc/yum.repos.d
 yum install -y java
-#yum install -y confluent-platform-oss-2.11
 yum install -y confluent-kafka-2.11
 
-## DO Droplet Metadata Service:https://www.digitalocean.com/community/tutorials/an-introduction-to-droplet-metadata
-# PRIVATE_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
-# PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-# HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
-## More generic, but sensitive to parsing
 PRIVATE_IP=$(ifconfig eth1 | grep "inet " | awk '{print $2}')
 PUBLIC_IP=$(ifconfig eth0 | grep "inet " | awk '{print $2}')
 HOSTNAME=$(hostname -s)
 REPLICA=${HOSTNAME#kafka}
+PUBLIC_HOSTNAME=$(hostname -s)
+PRIVATE_HOSTNAME="zk${REPLICA}.ztf.mjuric.org"
 
+# Make sure that zookeeper binds to the private IP only
 sed "s/\$PRIVATE_IP/$PRIVATE_IP/" config/zookeeper.properties > /etc/kafka/zookeeper.properties
+# Store this replica number
 echo "$REPLICA" > /var/lib/zookeeper/myid
 
-cp config/server.properties /etc/kafka
+# Kafka setup
+sed "s/\$REPLICA/$REPLICA/; s/\$PUBLIC_HOSTNAME/$PUBLIC_HOSTNAME/; s/\$PRIVATE_HOSTNAME/$PRIVATE_HOSTNAME/;" config/server.properties > /etc/kafka/server.properties
 
 #
 # Set up systemd services that will start mirrormaker and kafka
@@ -64,12 +77,13 @@ systemctl daemon-reload
 #
 # Enable and start it all up
 #
+exit
 
-#systemctl start confluent-zookeeper
-#systemctl enable confluent-zookeeper
-#
-#systemctl start confluent-kafka
-#systemctl enable confluent-kafka
-#
-#systemctl start ztf-mirrormaker
-#systemctl enable ztf-mirrormaker
+systemctl start confluent-zookeeper
+systemctl enable confluent-zookeeper
+
+systemctl start confluent-kafka
+systemctl enable confluent-kafka
+
+systemctl start ztf-mirrormaker
+systemctl enable ztf-mirrormaker
