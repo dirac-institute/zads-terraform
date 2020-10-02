29aed7baff766f7baa2e2b70c8da98a6bce27d30
Christopher Phillips <46461409+CStarZero@users.noreply.github.com>
2020-10-02 14:50:51 -0700

    Update standard-config.sh

--- provisioning/common/standard-config.sh
+++ provisioning/common/standard-config.sh
@@ -52,15 +52,15 @@ yum install joe iftop screen bind-utils telnet git
 #
 # Install Kerberos Server & Setup Kerberos Database (Initial Deployment Only!!!)
 #
-yum install -y krb5-server && yum install -y krb5-workstation && mkdir /etc/keytabs
-
-cp kerberos_config/krb5.conf /etc/
-cp kerberos_config/kdc.conf /var/kerberos/krb5kdc/kdc.conf
-cp kerberos_config/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
-
-cp config/kafka_server_jaas.conf /etc/kafka/
-cp config/kafka_client_jaas.conf /opt/ztf/etc/
-cp config/zookeeper_jaas.conf /etc/kafka/
+#yum install -y krb5-server && yum install -y krb5-workstation && mkdir /etc/keytabs
+#
+#cp kerberos_config/krb5.conf /etc/
+#cp kerberos_config/kdc.conf /var/kerberos/krb5kdc/kdc.conf
+#cp kerberos_config/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
+#
+#cp config/kafka_server_jaas.conf /etc/kafka/
+#cp config/kafka_client_jaas.conf /opt/ztf/etc/
+#cp config/zookeeper_jaas.conf /etc/kafka/
 
 
 #
